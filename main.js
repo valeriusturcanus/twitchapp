8347686f5ddefba118d967a30650fa81fa298848
valeriusturcanus <valeratrcn@gmail.com>
2017-06-14 14:03:44 +0100

    improved desing and bootstrap added

--- main.js
+++ main.js
@@ -21,24 +21,25 @@ $(document).ready(function() {
     getStrem();
 
     function fillAll(data) {
-        var divisions = "<div> <a href='"+  "https://www.twitch.tv/" + channels[counter]+ "'>%data%</a></div>";
-        var elementListed = "<h2" + " id='" + channels[counter] + "'>" + channels[counter] + "</h2>";
+        var divisions = "<div class='row'><a href='"+  "https://www.twitch.tv/" + channels[counter]+ "'>%data%</a></div>";
+        var elementListed = "<div class='col-sm-6'><h2" + " id='" + channels[counter] + "'>" + channels[counter] + "</h2></div>";
         if (data.stream) {
-            var contentStreamed = "<p class = 'online'>" + data.stream.channel.game + "</p>";
+            var contentStreamed = "<div class='col-sm-6'><p class = 'online'>" + data.stream.channel.game + "</p></div>";
             var newDivisions = divisions.replace("%data%", elementListed + contentStreamed);
             $("#All").append(newDivisions);
             $("#Online").append(newDivisions);
             counter++
         } else {
-            var newDivisions = divisions.replace("%data%", elementListed);
+            var contentStreamed = "<div class='col-sm-6'><p>Offline</p></div>";
+            var newDivisions = divisions.replace("%data%", elementListed + contentStreamed);
             $("#All").append(newDivisions);
             $("#Offline").append(newDivisions);
             counter++;
 
         }
     }
-    $("button").click(function(evt) {
-        $("button").removeClass("active");
+    $(".tablink").click(function(evt) {
+        $(".tablink").removeClass("active");
         evt.currentTarget.className += " active";
         var tabContentSelector = $(evt.currentTarget).text();
         $(".tabContent").addClass("doNotDisplay");
@@ -49,7 +50,7 @@ $(document).ready(function() {
         var valueSearched = ($(".searchBar").val()).toLowerCase();
         var tabContentActive = $("#" + $(".active").text()).children();
         for (var i = 0; i < tabContentActive.length; i++) {
-            var streamId = (tabContentActive[i].children[0].children[0].id).toLowerCase();
+            var streamId = (tabContentActive[i].children[0].children[0].children[0].id).toLowerCase();
             $(tabContentActive[i]).addClass("doNotDisplay");
             if (valueSearched == streamId.substring(0, valueSearched.length)) {
                 $(tabContentActive[i]).removeClass("doNotDisplay");
